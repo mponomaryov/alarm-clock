a9c775bbe6fb1dc41ae7364efd694829d5289242
Mihail Ponomaryov <mihail.ponomaryov@gmail.com>
2021-03-30 16:11:17 +0300

    initial

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-app>
+        <v-app-bar
+            app
+            dark
+            color="red"
+        >
+            <v-icon x-large>mdi-alarm</v-icon>
+            <v-toolbar-title>Alarm Clock</v-toolbar-title>
+            <v-spacer />
+            <v-btn icon>
+                <v-icon>mdi-cog</v-icon>
+            </v-btn>
+        </v-app-bar>
+
+        <v-main>
+            <v-container />
+        </v-main>
+    </v-app>
+</template>
+
+<script>
+export default {
+    name: 'App',
+
+    data: () => ({}),
+}
+</script>
