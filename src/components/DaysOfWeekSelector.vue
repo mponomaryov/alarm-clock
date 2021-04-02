6014bf1251c9caa92d5cbaf5ecd346cdcd4e66d0
Mihail Ponomaryov <mihail.ponomaryov@gmail.com>
2021-04-02 13:38:49 +0300

    days of week selector component

---
+++ src/components/DaysOfWeekSelector.vue
@@ -0,0 +1,48 @@
+<template>
+    <v-slide-group
+        multiple
+        :show-arrows="false"
+        :value="selectedDays"
+        @change="change"
+    >
+        <v-slide-item
+            v-for="(day, index) in 'mtwtfss'"
+            v-slot:default="{ active, toggle }"
+            :key="index"
+        >
+            <v-btn
+                icon
+                @click="toggle"
+            >
+                <v-icon large>
+                    {{
+                        active
+                            ? `mdi-alpha-${day}-circle`
+                            : `mdi-alpha-${day}-circle-outline`
+                    }}
+                </v-icon>
+            </v-btn>
+        </v-slide-item>
+    </v-slide-group>
+</template>
+
+<script>
+export default {
+    name: 'DaysOfWeekSelector',
+
+    props: {
+        selectedDays: {
+            type: Array,
+            default: () => [],
+        },
+    },
+
+    methods: {
+        change(value) {
+            value = [...value]
+            value.sort()
+            this.$emit('change', value)
+        },
+    },
+}
+</script>
