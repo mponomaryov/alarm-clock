1d986b776435359e26e938f565bee4e068fa53d6
Mihail Ponomaryov <mihail.ponomaryov@gmail.com>
2021-04-03 01:15:08 +0300

    vuetify-dialog setup

--- src/main.js
+++ src/main.js
@@ -2,9 +2,19 @@ import Vue from 'vue'
 
 import vuetify from './plugins/vuetify'
 import store from './store'
+import VuetifyDialog from 'vuetify-dialog'
 import App from './App.vue'
 
+import 'vuetify-dialog/dist/vuetify-dialog.css'
+
 Vue.config.productionTip = false
+
+Vue.use(VuetifyDialog, {
+    context: {
+        store,
+        vuetify,
+    },
+})
 
 new Vue({
     store,
